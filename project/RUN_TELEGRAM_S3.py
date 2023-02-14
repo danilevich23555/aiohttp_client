78b54de748456948228d1f74f9ecb2c96d947f67
ndolgov <ndolgov@ntv.ru>
2023-02-14 14:35:19 +0300

    continue

--- project/RUN_TELEGRAM_S3.py
+++ project/RUN_TELEGRAM_S3.py
@@ -8,12 +8,8 @@ from dcs import SendMessageResponse, GetUpdatesResponse, Message
 from s3 import S3Client
 
 async def cli():
-    cr = dict(
-        endpoint_url=config('DSN_MINIO'),
-        aws_secret_access_key=config('MINIO_ACCESS_KEY'),
-        aws_access_key_id=config('MINIO_SECRET_KEY')
-    )
-    s3cli = S3Client(**cr)
+    result_path = []
+
     async with TgClientWithFile(config('TELEGRAM_TOKEN')) as tg_cli:
         res = await tg_cli.get_updates()
         for x in res['result']:
@@ -21,14 +17,24 @@ async def cli():
                 r = Message.Schema().load(x['message'])
                 for k in r.photo:
                     res_path = await tg_cli.get_file(k['file_id'])
-                    await s3cli.fetch_and_upload('tests', f'{res_path.file_path[7:]}',
-                                        f'{tg_cli.API_FILE_PATH}{tg_cli.token}/{res_path.file_path}')
+                    result_path.append(res_path)
+
             except TypeError:
                 res_path = await tg_cli.get_file(r.document['file_id'])
-                await s3cli.fetch_and_upload('tests', f'{r.document["file_name"]}',
-                                             f'{tg_cli.API_FILE_PATH}{tg_cli.token}/{res_path.file_path}')
+                result_path.append(res_path)
 
+        return result_path
 
+async def run_uploader():
+    # todo чтение из файла который был записан в cli() либо чтение из очереди
+    cr = dict(
+        endpoint_url=config('DSN_MINIO'),
+        aws_secret_access_key=config('MINIO_ACCESS_KEY'),
+        aws_access_key_id=config('MINIO_SECRET_KEY')
+    )
+    s3cli = S3Client(**cr)
+    await s3cli.fetch_and_upload('tests', f'{res_path.file_path[7:]}',
+                                 f'{tg_cli.API_FILE_PATH}{tg_cli.token}/{res_path.file_path}')
 
 
 
@@ -41,7 +47,7 @@ async def worker():
     s3cli = S3Client(**cr)
     await s3cli.cr_bucket("tests")
     while True:
-        await cli()
+        await run_uploader()
 
 
 if __name__ == '__main__':
